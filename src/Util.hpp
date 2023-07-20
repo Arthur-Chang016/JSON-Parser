ca3f858467b6c3802aef8bb9d2d250fb6d573368
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2023-07-20 15:56:07 -0700

    Build closure correspondence map

--- src/Util.hpp
+++ src/Util.hpp
@@ -22,3 +22,38 @@ void TrimFile(string &s) {
         return c == ' ' || c == '\t' || c == '\n';
     }), end(s));
 }
+
+void Error(string s) {
+    cout << "Parse error: " << s << endl;
+    exit(1);
+}
+
+vector<int> BuildNextClosure(const string &s) {
+    vector<int> ret(s.size(), -1);
+    vector<pair<int, char>> st;
+    
+    auto correspond = [](char a, char b)->bool {
+        return (a == '\"' && b == '\"') || (a == '[' && b == ']') || (a == '{' && b == '}');
+    };
+    
+    for(int i = 0; i < (int)ret.size(); ++i) {
+        char cur = s[i];
+        if(cur == '\"') {
+            if(st.empty() || st.back().second != '\"') st.emplace_back(i, cur);
+            else {
+                auto [prev, c] = st.back();  st.pop_back();
+                ret[prev] = i;
+            } 
+        }
+        if(cur == '[' || cur == '{') {
+            st.emplace_back(i, cur);
+        } else if(cur == ']' || cur == '}') {
+            if(st.empty()) Error("no left correspondence: index " + to_string(i));
+            auto [prev, c] = st.back();  st.pop_back();
+            if(correspond(c, cur) == false) Error(string("not correspond: ") + to_string(prev) + c + ", " + to_string(i) + cur);
+            ret[prev] = i;
+        }
+    }
+    if(!st.empty()) Error("left thing in the stack. something not closed");
+    return ret;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,41 @@ using namespace std;
 #include "def.hpp"
 #include "Util.hpp"
 
+
+
 int main(int args, char **argv) {
     // check args
     if(args < 2) {
         cerr << "No input file" << endl;
         return 1;
     }
-    
-    // build corresponding char map
-    
-    vector<int> next;
-    
+    // file input and trim
     string file = ReadFile(argv[1]);
-    
     TrimFile(file);
     
+    if(file.empty()) {
+        cerr << "Empty File" << endl;
+        return 1;
+    }
+    
     cout << file << endl;
+    // cout << file.substr(310, 10) << endl;
+    
+    // build corresponding char map
+    vector<int> next = BuildNextClosure(file);
+    if(next.empty()) {
+        cerr << "Parse Error" << endl;
+        return 1;
+    }
+    
+    for(int i = 0; i < (int)next.size(); ++i) {
+        if(next[i] == -1) continue;
+        int ne = next[i];
+        cout << i << ' ' << file[i] << ", " << ne << ' ' << file[ne] << endl;
+    }
     
     
-    // string inputFile = 
     
-    // string_view sv;
-    
-    // cout << "hi" << endl;
     
     return 0;
 }
